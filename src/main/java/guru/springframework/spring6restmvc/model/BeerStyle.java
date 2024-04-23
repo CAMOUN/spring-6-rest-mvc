76ca837e63e88f2016394b826b6a5578081f151c
Camille BERNARD <cabernard@klanik.com>
2024-04-23 14:34:50 +0200

    Introduction to Lombok

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6restmvc.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
